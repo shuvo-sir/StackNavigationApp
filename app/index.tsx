d50aaecb171271ad3737e2b31a9c48848dfbe77d
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-04 17:00:24 +0600

    Stack Navigation

--- app/index.tsx
+++ app/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>My name is Shuvo </Text>
       <Link href={"/about"}>About</Link>
     </View>
   );
